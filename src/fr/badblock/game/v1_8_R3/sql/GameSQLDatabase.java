b8fbfbe63d5337065cbab98445ca70f2ec2c969a
xMalware <Aurelian User@Aurelian>
2016-06-01 01:07:11 +0200

    SQL :o

--- src/fr/badblock/game/v1_8_R3/sql/GameSQLDatabase.java
+++ src/fr/badblock/game/v1_8_R3/sql/GameSQLDatabase.java
@@ -6,94 +6,105 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 
 import com.google.common.collect.Queues;
 
 import fr.badblock.gameapi.databases.SQLDatabase;
+import fr.badblock.gameapi.databases.SQLRequestType;
 import fr.badblock.gameapi.utils.general.Callback;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class GameSQLDatabase implements SQLDatabase {
+	
 	private final String hostname, port, username, password, database;
+	private final int    threads = 16;
+	
 	protected Connection connection;
-	
 	protected Queue<String> toSave = Queues.newLinkedBlockingDeque();
+	protected List<SQLThread> sqlThread = new ArrayList<>();
 	
-	protected Thread saving = new Thread(){
+	protected Thread saving = new Thread() {
 		@Override
 		public void run(){
-			while(true){
-				String update = null;
-				
-				while((update = toSave.poll()) != null){
+			synchronized (saving) {
+				while(true){
+					String update = null;
+					while((update = toSave.poll()) != null){
+						try {
+							update(update, true);
+						} catch(Exception e){
+							e.printStackTrace();
+						}
+					}
 					try {
-						update(update, true);
-					} catch(Exception e){
+						saving.wait();
+					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 				}
-				
-				try {
-					Thread.sleep(500L);
-				} catch (InterruptedException e){}
 			}
 		}
 	};
-	
+
 	{
 		saving.start();
+		for (int i = 0; i < threads; i++) 
+			sqlThread.add(new SQLThread(i));
+			
 	}
-	
+
 	public Connection openConnection() throws SQLException, ClassNotFoundException {
 		if (checkConnection()) {
 			return this.connection;
 		}
-		
+
 		Class.forName("com.mysql.jdbc.Driver");
 		this.connection = DriverManager.getConnection("jdbc:mysql://" + 
 				this.hostname + ":" + this.port + "/" + this.database + "?autoReconnect=true", this.username, this.password);
-		
+
 		return this.connection;
 	}
-	
+
 	public boolean checkConnection() throws SQLException {
 		return (this.connection != null) && (!this.connection.isClosed());
 	}
-	
+
 	public void closeConnection() throws SQLException {
 		if(checkConnection());
-			connection.close();
+		connection.close();
 	}
-	
+
 	@Override
 	public Statement createStatement() throws Exception {
 		if(!checkConnection()) {
 			openConnection();
 		}
-		
+
 		return connection.createStatement();
 	}
-	
+
 	@Override
 	public PreparedStatement preparedStatement(String request) throws Exception {
 		if(!checkConnection()) {
 			openConnection();
 		}
-		
+
 		return connection.prepareStatement(request);
 	}
-	
+
 	@Override
 	public ResultSet query(String request) throws Exception {
 		if(!checkConnection()) {
 			openConnection();
 		}
-		
+
 		Statement statement = createStatement();
 		statement.closeOnCompletion();
-		
+
 		ResultSet result = statement.executeQuery(request);
 
 		return result;
@@ -106,13 +117,13 @@ public class GameSQLDatabase implements SQLDatabase {
 			public void run(){
 				ResultSet result = null;
 				Throwable error  = null;
-				
+
 				try {
 					result = query(request);
 				} catch (Throwable t) {
 					error  = t;
 				}
-				
+
 				callback.done(result, error);
 			}
 		}.start();
@@ -124,14 +135,33 @@ public class GameSQLDatabase implements SQLDatabase {
 			if(!checkConnection()) {
 				openConnection();
 			}
-			
+
 			Statement statement = createStatement();
 			statement.executeUpdate(request);
-			
+
 			statement.close();
 		} else {
 			toSave.add(request);
 		}
+	}
+
+	@Override
+	public void call(String request, SQLRequestType requestType) {
+		if (requestType.equals(requestType.QUERY)) throw new IllegalArgumentException("U can't done a query if you don't handle a callback!");
+		call(request, requestType, null);
+	}
+
+	@Override
+	public void call(String request, SQLRequestType requestType, Callback<ResultSet> callback) {
+		SQLThread availableThread = null;
+		SQLThread firstThread = null;
+		for (SQLThread sqlThread : sqlThread)
+			if (sqlThread.isAvailable()) {
+				availableThread = sqlThread;
+				break;
+			}else firstThread = sqlThread;
+		if (availableThread != null) availableThread.call(new SQLRequest(requestType, request, callback));
+		else if (firstThread != null) firstThread.call(new SQLRequest(requestType, request, callback));
 	}
 
 }
---
+++ src/fr/badblock/game/v1_8_R3/sql/SQLThread.java
@@ -0,0 +1,47 @@
+package fr.badblock.game.v1_8_R3.sql;
+
+import java.util.Queue;
+
+import com.google.common.collect.Queues;
+
+/**
+ * @author xMalware
+ */
+public class SQLThread extends Thread {
+	
+	private Queue<SQLRequest>	requests	= Queues.newLinkedBlockingDeque();
+	
+	public SQLThread(int id) {
+		this.setName("BadBlockAPI/SQL/" + id);
+		this.start();
+	}
+	
+	@Override
+	public void run() {
+		synchronized (this) {
+			while (true) {
+				if (!requests.isEmpty()) {
+					SQLRequest request = requests.poll();
+					request.done();
+				}
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public boolean isAvailable() {
+		return this.getState() != null && this.getState().equals(State.WAITING);
+	}
+
+	public void call(SQLRequest sqlRequest) {
+		requests.add(sqlRequest);
+		synchronized (this) {
+			this.notify();
+		}
+	}
+	
+}
