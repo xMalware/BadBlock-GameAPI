bebd77411da3241d63400ca701cbc18d8bca3b3a
xMalware <Aurelian User@Aurelian>
2016-06-09 11:53:28 +0200

    Et les recipients c'est pas fait pour les chiens !

--- src/fr/badblock/game/v1_8_R3/listeners/ChatListener.java
+++ src/fr/badblock/game/v1_8_R3/listeners/ChatListener.java
@@ -21,45 +21,47 @@ public class ChatListener extends BadListener {
 	@EventHandler(priority=EventPriority.HIGHEST,ignoreCancelled=true)
 	public void onChat(AsyncPlayerChatEvent e){
 		if(!enabled) return;
-		
+
 		e.setCancelled(true);
-		
+
 		BadblockPlayer player = (BadblockPlayer) e.getPlayer();
-		
+
 		if(!player.hasPermission(GamePermission.ADMIN)){
-			
+
 			String temp = ChatColor.translateAlternateColorCodes('&', e.getMessage());
 			temp		= ChatColor.stripColor(temp);
-		
+
 			e.setMessage(temp);
-			
+
 		}
-		
+
 		if(player.getBadblockMode() == BadblockMode.SPECTATOR){
 			TranslatableString result = new TranslatableString("chat.spectator" + (custom == null ? "" : "." + custom), player.getName(), player.getGroupPrefix(), e.getMessage(), player.getPlayerData().getLevel());
-		
-			for(Player p : Bukkit.getOnlinePlayers()){
+
+			for(Player p : e.getRecipients()){
 				BadblockPlayer bPlayer = (BadblockPlayer) p;
-				
+
 				if(bPlayer.getBadblockMode() == BadblockMode.SPECTATOR){
 					result.send(bPlayer);
 				}
 			}
 		} else if(team && player.getTeam() != null && e.getMessage().startsWith("$")){
 			TranslatableString result = new TranslatableString("chat.team" + (custom == null ? "" : "." + custom), player.getName(), player.getGroupPrefix(), player.getTeam().getChatName(), e.getMessage().substring(1), player.getPlayerData().getLevel());
-			
+
 			for(BadblockPlayer p : player.getTeam().getOnlinePlayers()){
-				result.send(p);
+				if (e.getRecipients().contains(p))
+					result.send(p);
 			}
 		} else {
 			Object team = "";
-			
+
 			if(player.getTeam() != null)
 				team = player.getTeam().getChatPrefix();
-			
+
 			TranslatableString s = new TranslatableString("chat.player" + (custom == null ? "" : "." + custom), player.getName(), player.getGroupPrefix(), team, e.getMessage(), player.getPlayerData().getLevel());
-			s.broadcast();
+			for (Player pl : e.getRecipients())
+				s.send(pl);
 		}
-		
+
 	}
 }
