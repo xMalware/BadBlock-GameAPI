851ec7d5d73dae0d1ae145be37e206c9682e1e53
LeLanN <audrandoublet@gmail.com>
2016-08-12 17:36:36 +0200

    #çaVaPasMarcher

--- src/fr/badblock/game/v1_8_R3/players/utils/OtherPermissions.java
+++ src/fr/badblock/game/v1_8_R3/players/utils/OtherPermissions.java
@@ -4,14 +4,20 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import ru.tehkode.permissions.bukkit.PermissionsEx;
-
 public class OtherPermissions {
 	public static boolean has(Player player, String perm){
 		Plugin plugin = Bukkit.getPluginManager().getPlugin("PermissionsEx");
 		
 		if(plugin != null){
-			return PermissionsEx.getUser(player).has(perm);
+			
+			try {
+				Class<?> permissionExClass = Class.forName("ru.tehkode.permissions.bukkit.PermissionsEx");
+				
+				Object result = permissionExClass.getMethod("getUser", Player.class).invoke(null, player);
+				return (boolean) result.getClass().getMethod("has", String.class).invoke(result, perm);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		
 		return false;
