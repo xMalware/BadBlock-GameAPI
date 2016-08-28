861e878aee4f0b33f6aca528576a5d2b5a1efb24
xMalware <xmalware2@gmail.com>
2016-08-28 21:52:27 +0100

    Import

--- src/fr/badblock/game/core18R3/GamePlugin.java
+++ src/fr/badblock/game/core18R3/GamePlugin.java
@@ -61,6 +61,7 @@ import fr.badblock.game.core18R3.commands.TeleportCommand;
 import fr.badblock.game.core18R3.commands.UpCommand;
 import fr.badblock.game.core18R3.commands.VanishCommand;
 import fr.badblock.game.core18R3.commands.WandCommand;
+import fr.badblock.game.core18R3.commands.WeatherCommand;
 import fr.badblock.game.core18R3.commands.WhitelistCommand;
 import fr.badblock.game.core18R3.commands.WorkbrenchCommand;
 import fr.badblock.game.core18R3.configuration.GameConfiguration;
