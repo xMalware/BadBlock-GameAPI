6571f969e716fed8d812c80d94d3ebe4cec2a859
LeLanN <audrandoublet@gmail.com>
2016-08-28 22:23:14 +0200

    Remove f*cking invists

--- src/fr/badblock/game/core18R3/commands/TeleportAllCommand.java
+++ src/fr/badblock/game/core18R3/commands/TeleportAllCommand.java
@@ -9,7 +9,6 @@ import fr.badblock.gameapi.GameAPI;
 import fr.badblock.gameapi.command.AbstractCommand;
 import fr.badblock.gameapi.players.BadblockPlayer;
 import fr.badblock.gameapi.players.BadblockPlayer.GamePermission;
-import fr.badblock.gameapi.utils.BukkitUtils;
 import fr.badblock.gameapi.utils.i18n.TranslatableString;
 
 public class TeleportAllCommand extends AbstractCommand {
@@ -30,7 +29,7 @@ public class TeleportAllCommand extends AbstractCommand {
 		return true;
 	}
 	
-	private boolean tppos(CommandSender sender, String[] args){
+	protected boolean tppos(CommandSender sender, String[] args){
 		BadblockPlayer player = null;
 		
 		int begin = 0;
