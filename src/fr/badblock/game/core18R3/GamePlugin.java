545d1fb2ad7e31f0b7d9fb87a66c23acc3fcabff
LeLanN <audrandoublet@gmail.com>
2016-08-28 22:27:50 +0200

    Dafuck

--- src/fr/badblock/game/core18R3/GamePlugin.java
+++ src/fr/badblock/game/core18R3/GamePlugin.java
@@ -45,6 +45,7 @@ import fr.badblock.game.core18R3.commands.KillallCommand;
 import fr.badblock.game.core18R3.commands.KitsCommand;
 import fr.badblock.game.core18R3.commands.LagCommand;
 import fr.badblock.game.core18R3.commands.ListCommand;
+import fr.badblock.game.core18R3.commands.PingCommand;
 import fr.badblock.game.core18R3.commands.PortalCommand;
 import fr.badblock.game.core18R3.commands.RepairCommand;
 import fr.badblock.game.core18R3.commands.ShopCommand;
