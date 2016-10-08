0e1d97ef4f07740aec31a9d6f345cfdcff2c552d
LeLanN <audrandoublet@gmail.com>
2016-10-08 15:54:39 +0200

    implement NamedEntitySpawn packet

--- src/fr/badblock/game/core18R3/packets/out/GamePlayNamedEntitySpawn.java
+++ src/fr/badblock/game/core18R3/packets/out/GamePlayNamedEntitySpawn.java
@@ -3,36 +3,73 @@ package fr.badblock.game.core18R3.packets.out;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
-import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.Material;
 
 import fr.badblock.game.core18R3.packets.GameBadblockOutPacket;
+import fr.badblock.game.core18R3.watchers.GameWatcherEntity;
 import fr.badblock.gameapi.packets.out.play.PlayNamedEntitySpawn;
+import fr.badblock.gameapi.packets.watchers.WatcherEntity;
 import fr.badblock.gameapi.utils.reflection.Reflector;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import net.minecraft.server.v1_8_R3.DataWatcher;
+import net.minecraft.server.v1_8_R3.MathHelper;
 import net.minecraft.server.v1_8_R3.Packet;
 import net.minecraft.server.v1_8_R3.PacketPlayOutNamedEntitySpawn;
+import net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntityLiving;
 
 @NoArgsConstructor@Data
 @EqualsAndHashCode(callSuper=false)
 @Accessors(chain = true, fluent = false)
 public class GamePlayNamedEntitySpawn extends GameBadblockOutPacket implements PlayNamedEntitySpawn {
-	private Player player = null;
-
+	private int 			   entityId 	= 0;
+	private UUID			   uniqueId		= UUID.randomUUID();
+	private Location		   location 	= null;
+	private Material		   itemInHand   = Material.AIR;
+	private WatcherEntity	   watchers		= null;
+	private DataWatcher		   tempWatcher  = null;
+	
+	@SuppressWarnings("deprecation")
 	public GamePlayNamedEntitySpawn(PacketPlayOutNamedEntitySpawn packet){
 		Reflector reflector = new Reflector(packet);
 		
 		try {
-			UUID uniqueId   = (UUID) reflector.getFieldValue("b");
-			player			= Bukkit.getPlayer(uniqueId);
+			entityId     = (int) reflector.getFieldValue("a");
+			uniqueId     = (UUID) reflector.getFieldValue("b");
+			location	 = new Location(Bukkit.getWorlds().get(0),
+							(double) reflector.getFieldValue("c") / 32.0D,
+							(double) reflector.getFieldValue("d") / 32.0D,
+							(double) reflector.getFieldValue("e") / 32.0D,
+							(float)  reflector.getFieldValue("f") / 256.0F * 360.0F,
+							(float)  reflector.getFieldValue("g") / 256.0F * 360.0F);
+			itemInHand	 = Material.getMaterial((int) reflector.getFieldValue("h"));
+			tempWatcher	 = (DataWatcher) reflector.getFieldValue("i");
 		} catch(Exception e){}
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public Packet<?> buildPacket() throws Exception {
-		return new PacketPlayOutNamedEntitySpawn(((CraftPlayer) player).getHandle());
-	}
+		PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving();
+		Reflector reflector = new Reflector(packet);
+		
+		reflector.setFieldValue("a", entityId);
+		reflector.setFieldValue("b", uniqueId);
+		reflector.setFieldValue("c", MathHelper.floor(location.getX() * 32.0D));
+		reflector.setFieldValue("d", MathHelper.floor(location.getY() * 32.0D));
+		reflector.setFieldValue("e", MathHelper.floor(location.getZ() * 32.0D));
+		
+		reflector.setFieldValue("f", ((byte)(int)(location.getYaw() * 256.0F / 360.0F)));
+		reflector.setFieldValue("g", ((byte)(int)(location.getPitch() * 256.0F / 360.0F)));
+		reflector.setFieldValue("h", itemInHand.getId());
+		
+		if(watchers != null)
+			reflector.setFieldValue("i", ((GameWatcherEntity) watchers).convertToDatawatcher());
+		else reflector.setFieldValue("i", tempWatcher);
+		
+		return packet;
+	}	
 }
