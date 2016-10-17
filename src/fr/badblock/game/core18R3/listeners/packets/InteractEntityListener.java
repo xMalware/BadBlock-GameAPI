3e82a6f5a60dd11872c25ee374819389254014da
LeLanN <audrandoublet@gmail.com>
2016-10-17 17:31:51 +0200

    interact disguise -> player ;o

--- src/fr/badblock/game/core18R3/listeners/packets/InteractEntityListener.java
+++ src/fr/badblock/game/core18R3/listeners/packets/InteractEntityListener.java
@@ -1,8 +1,12 @@
 package fr.badblock.game.core18R3.listeners.packets;
 
+import java.util.stream.Collectors;
+
 import org.bukkit.Bukkit;
 
 import fr.badblock.game.core18R3.fakeentities.FakeEntities;
+import fr.badblock.game.core18R3.players.GameBadblockPlayer;
+import fr.badblock.gameapi.GameAPI;
 import fr.badblock.gameapi.events.PlayerFakeEntityInteractEvent;
 import fr.badblock.gameapi.fakeentities.FakeEntity;
 import fr.badblock.gameapi.packets.InPacketListener;
@@ -14,6 +18,17 @@ public class InteractEntityListener extends InPacketListener<PlayInUseEntity> {
 	@Override
 	public void listen(BadblockPlayer player, PlayInUseEntity packet) {
 		FakeEntity<?> entity = FakeEntities.retrieveFakeEntity(packet.getEntityId());
+		
+		if(entity != null){
+			for(BadblockPlayer pl : GameAPI.getAPI().getOnlinePlayers().stream().filter(p -> p.isDisguised()).collect(Collectors.toList())){
+				GameBadblockPlayer gpl = (GameBadblockPlayer) pl;
+				
+				if(gpl.getDisguiseEntity().getId() == entity.getId()){
+					packet.setEntityId(pl.getEntityId());
+					return;
+				}
+			}
+		}
 		
 		if (entity != null && player.getLastFakeEntityUsedTime() < System.currentTimeMillis()) {
 			packet.setCancelled(true);
--- src/fr/badblock/game/core18R3/packets/in/GamePlayInUseEntity.java
+++ src/fr/badblock/game/core18R3/packets/in/GamePlayInUseEntity.java
@@ -5,10 +5,15 @@ import fr.badblock.gameapi.packets.in.play.PlayInUseEntity;
 import fr.badblock.gameapi.utils.reflection.Reflector;
 import fr.badblock.gameapi.utils.selections.Vector3f;
 import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.server.v1_8_R3.Packet;
 import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity;
+import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity.EnumEntityUseAction;
 
 @Getter public class GamePlayInUseEntity extends GameBadblockInPacket implements PlayInUseEntity {
+	@Setter
 	private int 			entityId;
+	@Setter
 	private UseEntityAction action;
 	private Vector3f 		targetPosition;
 	
@@ -25,5 +30,18 @@ import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity;
 		try {
 			this.entityId   = (int) reflector.getFieldValue("a");
 		} catch (Exception e){}
+	}
+
+	@Override
+	public Packet<?> toNms(){
+		PacketPlayInUseEntity packet    = (PacketPlayInUseEntity) super.toNms();
+		Reflector 			  reflector = new Reflector(packet);
+		
+		try {
+			reflector.setFieldValue("a", entityId);
+			reflector.setFieldValue("action", EnumEntityUseAction.valueOf( action.name() ));
+		} catch (Exception e){}
+		
+		return new PacketPlayInUseEntity();
 	}
 }
