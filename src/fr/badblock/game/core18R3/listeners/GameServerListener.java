aa56f61dc985b7079e3f7f092e99b1bfa168725a
xMalware <xmalware2@gmail.com>
2016-11-02 05:00:31 +0100

    Fix :D

--- src/fr/badblock/game/core18R3/listeners/GameServerListener.java
+++ src/fr/badblock/game/core18R3/listeners/GameServerListener.java
@@ -13,21 +13,22 @@ import fr.badblock.game.core18R3.gameserver.GameServerManager;
 import fr.badblock.game.core18R3.gameserver.threading.GameServerKeeperAliveTask;
 import fr.badblock.gameapi.BadListener;
 import fr.badblock.gameapi.GameAPI;
+import fr.badblock.gameapi.run.RunType;
 
 public class GameServerListener extends BadListener {
-	
+
 	@EventHandler (priority = EventPriority.LOWEST)
 	public void onJoin(PlayerJoinEvent event) {
 		// On renvoit les infos mises � jour, avec 0 joueurs en plus envoy� � Docker car lors du PlayerJoinEvent le joueur est d�j� list� dans la liste des joueurs connect�s
 		update(event.getPlayer(), 0, true);
 	}
-	
+
 	@EventHandler (priority = EventPriority.LOWEST)
 	public void onQuit(PlayerQuitEvent event) {
 		// On update, avec -1 joueurs en plus envoy� � Docker car lors du PlayerQuitEvent le joueur est toujours list� dans la liste des joueurs connect�s
 		update(event.getPlayer(), -1, false);
 	}
-	
+
 	public void update(Player player, int addedPlayers, boolean joinedMessage) {
 		GamePlugin gamePlugin = GamePlugin.getInstance();
 		// En mode test, pouf!
@@ -38,18 +39,20 @@ public class GameServerListener extends BadListener {
 		GameServerKeeperAliveTask gameServerKeeperAliveTask = gameServerManager.getGameServerKeeperAliveTask();
 		gameServerKeeperAliveTask.incrementJoinTime();
 		gameServerKeeperAliveTask.keepAlive();
-		
+
 		if (joinedMessage) {
 			GameAPI.i18n().sendMessage(player, "gameserver.join", serverName);
-			if (gamePlugin.getServerXpBonus() > 1 || gamePlugin.getServerBadcoinsBonus() > 1) {
-				player.playSound(player.getLocation(), Sound.LEVEL_UP, 100, 1);
-				player.sendMessage("§8§m---------------------------------------------");
-				player.sendMessage("§a§l✘ Événement actif ✘");
-				player.sendMessage("§8§m---------------------------------------------");
-				player.sendMessage("§a§l➤ §r§aBadCoins doublé par §b" + gamePlugin.getServerBadcoinsBonus());
-				player.sendMessage("§a§l➤ §r§aXP doublé par §b" + gamePlugin.getServerXpBonus());
-				player.sendMessage("§8§m---------------------------------------------");		
-				// TODO: pas sous i18n pour le moment la flemme
+			if (GameAPI.getAPI().getRunType().equals(RunType.GAME)) {
+				if (gamePlugin.getServerXpBonus() > 1 || gamePlugin.getServerBadcoinsBonus() > 1) {
+					player.playSound(player.getLocation(), Sound.LEVEL_UP, 100, 1);
+					player.sendMessage("§8§m---------------------------------------------");
+					player.sendMessage("§a§l✘ Événement actif ✘");
+					player.sendMessage("§8§m---------------------------------------------");
+					player.sendMessage("§a§l➤ §r§aBadCoins doublé par §b" + gamePlugin.getServerBadcoinsBonus());
+					player.sendMessage("§a§l➤ §r§aXP doublé par §b" + gamePlugin.getServerXpBonus());
+					player.sendMessage("§8§m---------------------------------------------");		
+					// TODO: pas sous i18n pour le moment la flemme
+				}
 			}
 		}
 	}
