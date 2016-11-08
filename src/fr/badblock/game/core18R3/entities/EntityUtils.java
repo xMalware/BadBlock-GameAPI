aee45ffdd8d7c7cb4740ef6e7cb7c368277c4ee4
xMalware <xmalware2@gmail.com>
2016-11-08 10:53:23 +0100

    fix NPE

--- src/fr/badblock/game/core18R3/entities/EntityUtils.java
+++ src/fr/badblock/game/core18R3/entities/EntityUtils.java
@@ -30,7 +30,7 @@ public class EntityUtils {
 		EntityInsentient entity = creature.getNMSEntity();
 
 		creature.setNormalController(entity.getControllerMove());
-		
+
 		if(entity.isFireProof())
 			creature.addCreatureFlag(CreatureFlag.FIREPROOF);
 
@@ -41,26 +41,26 @@ public class EntityUtils {
 		creature.setCreatureBehaviour(creature instanceof EntityBat ? CreatureBehaviour.FLYING : CreatureBehaviour.NORMAL);
 		creature.regenerateAttributes();
 	}
-	
+
 	private static IAttribute getAttribute(CreatureGenericAttribute from){
 		switch(from){
-			case DAMAGE:
-				return GenericAttributes.ATTACK_DAMAGE;
-			case SPEED:
-				return GenericAttributes.MOVEMENT_SPEED;
+		case DAMAGE:
+			return GenericAttributes.ATTACK_DAMAGE;
+		case SPEED:
+			return GenericAttributes.MOVEMENT_SPEED;
 		};
-		
+
 		return null;
 	}
-	
+
 	public static double getAttributeValue(NMSCustomCreature creature, CreatureGenericAttribute attribute){
 		return creature.getNMSEntity().getAttributeInstance(getAttribute(attribute)).getValue();
 	}
-	
+
 	public static void setAttributeValue(NMSCustomCreature creature, CreatureGenericAttribute attribute, double value){
 		creature.getNMSEntity().getAttributeInstance(getAttribute(attribute)).setValue(value);
 	}
-	
+
 	public static void regen(NMSCustomCreature creature){
 		try {
 			new Reflector(creature.getNMSEntity()).setFieldValue("fireProof", creature.hasCreatureFlag(CreatureFlag.FIREPROOF));
@@ -69,10 +69,10 @@ public class EntityUtils {
 		};
 		creature.getNMSEntity().setInvisible(creature.hasCreatureFlag(CreatureFlag.INVISIBLE));
 	}
-	
+
 	public static void move(NMSCustomCreature creature, float sideMot, float forMot){
 		EntityInsentient entity = creature.getNMSEntity();
-		
+
 		if(creature.getCreatureBehaviour() == CreatureBehaviour.MOTIONLESS)
 			return;
 
@@ -130,13 +130,13 @@ public class EntityUtils {
 		entity.aF += (f3 - entity.aF) * 0.4F;
 		entity.aG += entity.aF;
 	}
-	
+
 	public static void movePassenger(NMSCustomCreature creature){
 		EntityInsentient entity = creature.getNMSEntity();
-		
+
 		entity.lastYaw = entity.yaw = entity.passenger.yaw;
 		entity.pitch = entity.passenger.pitch * 0.5F;
-		
+
 		entity.aI = entity.aG = entity.yaw;
 
 		float sideMot = ((EntityLiving) entity.passenger).aZ * 0.5F;
@@ -149,7 +149,7 @@ public class EntityUtils {
 		sideMot *= 0.75F;
 		sideMot *= creature.getSpeed();
 		forMot *= creature.getSpeed();
-		
+
 		if(creature.getCreatureBehaviour() != CreatureBehaviour.FLYING){
 			Field jump = null; //Jumping
 			try {
@@ -167,8 +167,10 @@ public class EntityUtils {
 
 			if (jump != null && entity.onGround) {
 				try {
-					if (jump.getBoolean(entity.passenger)) {
-						entity.motY = 0.5d;
+					if (entity.passenger != null) {
+						if (jump.getBoolean(entity.passenger)) {
+							entity.motY = 0.5d;
+						}
 					}
 				} catch (IllegalAccessException e) {
 					e.printStackTrace();
@@ -179,7 +181,7 @@ public class EntityUtils {
 		} else entity.motY = -entity.pitch/45;
 
 		entity.a(sideMot, forMot, (float) creature.getSpeed());
-		
+
 		if(sideMot != 0 || forMot != 0) {
 			entity.move(entity.motX, entity.motY, entity.motZ);
 
@@ -188,7 +190,7 @@ public class EntityUtils {
 			entity.motZ *= 0.8D;
 		}
 	}
-	
+
 	public static void moveFlying(NMSCustomCreature creature){
 		EntityInsentient entity = creature.getNMSEntity();
 
@@ -199,7 +201,7 @@ public class EntityUtils {
 		if(entity.passenger != null || entity.getGoalTarget() != null){
 			return;	
 		}
-		
+
 		creature.callSuperMoveFlying();
 
 		if(creature.getCreatureBehaviour() != CreatureBehaviour.FLYING)
@@ -207,14 +209,14 @@ public class EntityUtils {
 
 		BlockPosition position = creature.getBlockPosition();
 		Random		  random   = creature.getRandom();
-		
+
 		if((position == null) || (random.nextInt(30) == 0) || (position.c((int) entity.locX, (int) entity.locY, (int) entity.locZ) < 4.0F)) {
 			position = new BlockPosition((int) entity.locX + random.nextInt(7) - random.nextInt(7),
 					(int) entity.locY + random.nextInt(6) - 2,
 					(int) entity.locZ + random.nextInt(7) - random.nextInt(7));
 			creature.setBlockPosition(position);
 		}
-		
+
 		double d1 = position.getX() + 0.5D - entity.locX;
 		double d2 = position.getY() + 0.1D - entity.locY;
 		double d3 = position.getZ() + 0.5D - entity.locZ;
@@ -229,17 +231,17 @@ public class EntityUtils {
 		creature.setBe(0.5f);
 		entity.yaw += f2;
 	}
-	
+
 	public static boolean rightClick(NMSCustomCreature creature, EntityHuman entityhuman){
 		EntityInsentient entity = creature.getNMSEntity();
 
 		if(!creature.hasCreatureFlag(CreatureFlag.RIDEABLE)){
 			return creature.callSuperRightClick(entityhuman);
 		}
-		
+
 		if(creature.callSuperRightClick(entityhuman))
 			return true;
-		
+
 		if(!entity.world.isClientSide && (entity.passenger == null || entity.passenger == entityhuman)) {
 			entityhuman.mount(entity);
 			return true;
@@ -247,28 +249,28 @@ public class EntityUtils {
 
 		return false;
 	}
-	
+
 	public static boolean damageEntity(NMSCustomCreature creature, DamageSource damagesource, float f){
 		if(creature.getEntityType().isHostile()){
 			return creature.callSuperDamageEntity(damagesource, f);
 		}
-		
+
 		if(creature.getNMSEntity().isInvulnerable(damagesource))
 			return false;
-		
+
 		if(creature.callSuperDamageEntity(damagesource, f)){
 			Entity entity = damagesource.getEntity();
 			return entity != null && (entity.passenger != entity) && (entity.vehicle != entity);
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean attack(NMSCustomCreature creature, Entity entity){
 		if(!creature.hasCreatureFlag(CreatureFlag.AGRESSIVE)) return false;
-		
+
 		EntityInsentient thisEntity = creature.getNMSEntity();
-		
+
 		float f = (float) creature.getCreatureGenericAttribute(CreatureGenericAttribute.DAMAGE);
 
 		int i = 0;
@@ -276,9 +278,9 @@ public class EntityUtils {
 			f += EnchantmentManager.a(thisEntity.bA(), ((EntityLiving)entity).getMonsterType());
 			i += EnchantmentManager.a(thisEntity);
 		}
-		
+
 		boolean flag = entity.damageEntity(DamageSource.mobAttack(thisEntity), f);
-		
+
 		if (flag) {
 			if (i > 0) {
 				entity.g(-MathHelper.sin(thisEntity.yaw * 3.1415927F / 180.0F) * i * 0.5F, 0.1D, MathHelper.cos(thisEntity.yaw * 3.1415927F / 180.0F) * i * 0.5F);
@@ -293,27 +295,27 @@ public class EntityUtils {
 					entity.setOnFire(combustEvent.getDuration());
 				}
 			}
-			
-			
+
+
 			creature.callAEntity(thisEntity, entity);
 		}
-		
+
 		return flag;
 	}
-	
+
 	private static boolean isInLava(Entity entity){
 		return entity.world.a(entity.getBoundingBox().grow(-0.10000000149011612D, -0.4000000059604645D, -0.10000000149011612D), Material.LAVA);
 	}
-	
+
 	public static void doDrops(NMSCustomCreature creature){
 		if(creature.getCustomLoots() == null)
 			return;
-		
+
 		EntityInsentient ent = creature.getNMSEntity();
-		
+
 		for(ItemStack item : creature.getCustomLoots().apply(ent.bc())){
 			net.minecraft.server.v1_8_R3.ItemStack itemNms = CraftItemStack.asNMSCopy(item);
-			
+
 			ent.a(itemNms.getItem(), itemNms.count);
 		}
 	}
