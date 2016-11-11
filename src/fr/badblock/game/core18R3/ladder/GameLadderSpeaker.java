378e8ca293fa8c0d68276286e9c960253c426ba8
xMalware <xmalware2@gmail.com>
2016-11-11 01:49:12 +0100

    br

--- src/fr/badblock/game/core18R3/ladder/GameLadderSpeaker.java
+++ src/fr/badblock/game/core18R3/ladder/GameLadderSpeaker.java
@@ -13,12 +13,15 @@ import java.util.Map;
 import java.util.Queue;
 
 import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.Team;
 
 import com.google.common.collect.Queues;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import fr.badblock.game.core18R3.GamePlugin;
 import fr.badblock.game.core18R3.players.GameBadblockPlayer;
+import fr.badblock.gameapi.GameAPI;
 import fr.badblock.gameapi.databases.LadderSpeaker;
 import fr.badblock.gameapi.events.api.PlayerDataChangedEvent;
 import fr.badblock.gameapi.game.GameState;
@@ -233,14 +236,13 @@ public class GameLadderSpeaker implements LadderSpeaker, PacketHandler {
 
 			if(player != null){
 				Bukkit.getPluginManager().callEvent(new PlayerDataChangedEvent(player));
-				/*if (packet.getData().startsWith("{\"permissions\":")) {
+				if (packet.getData().startsWith("{\"permissions\":")) {
 					GameAPI gameAPI = GamePlugin.getAPI();
-					gameAPI.getServer().getPluginManager().callEvent(new PlayerPermissionLoadedEvent(player));
 					Team team = gameAPI.getBadblockScoreboard().getHandler().getEntryTeam(player.getName());
 					if (!team.getName().equals(player.getMainGroup())) team.removeEntry(player.getName());
 					gameAPI.getBadblockScoreboard().getHandler().getTeam(player.getMainGroup()).addEntry(player.getName());
-					Bukkit.getPluginManager().callEvent(new PlayerDataChangedEvent(player));
-				}*/
+					//Bukkit.getPluginManager().callEvent(new PlayerDataChangedEvent(player));
+				}
 			}
 		} else if(packet.getType() == DataType.IP && packet.getAction() == DataAction.SEND){
 			Callback<JsonObject> callback = requestedIps.get(packet.getKey().toLowerCase());
