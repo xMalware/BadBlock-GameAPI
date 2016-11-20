a0c48e2ec8c88ceefaf1fe5765896ede65496c0d
xMalware <xmalware2@gmail.com>
2016-11-20 16:27:41 +0100

    fix

--- src/fr/badblock/game/core18R3/packets/in/GamePlayInFlying.java
+++ src/fr/badblock/game/core18R3/packets/in/GamePlayInFlying.java
@@ -1,16 +1,32 @@
 package fr.badblock.game.core18R3.packets.in;
 
 import fr.badblock.game.core18R3.packets.GameBadblockInPacket;
-import fr.badblock.gameapi.packets.in.play.PlayInPosition;
+import fr.badblock.gameapi.packets.in.play.PlayInFlying;
 import fr.badblock.gameapi.utils.selections.Vector3f;
 import lombok.Getter;
-import net.minecraft.server.v1_8_R3.PacketPlayInFlying.PacketPlayInPosition;
+import net.minecraft.server.v1_8_R3.PacketPlayInFlying;
 
-@Getter public class GamePlayInFlying extends GameBadblockInPacket implements PlayInPosition {
+@Getter public class GamePlayInFlying extends GameBadblockInPacket implements PlayInFlying {
 	private Vector3f position;
-
-	public GamePlayInFlying(PacketPlayInPosition packet){
+	private boolean  hasPosition;
+	private boolean  hasLook;
+	private boolean  onGround;
+	
+	public GamePlayInFlying(PacketPlayInFlying packet){
 		super(packet);
 		this.position = new Vector3f(packet.a(), packet.b(), packet.c());
+		this.hasPosition = packet.g();
+		this.hasLook = packet.h();
+		this.onGround = packet.f();
+	}
+
+	@Override
+	public boolean hasPosition() {
+		return this.hasPosition;
+	}
+
+	@Override
+	public boolean hasLook() {
+		return this.hasLook;
 	}
 }
