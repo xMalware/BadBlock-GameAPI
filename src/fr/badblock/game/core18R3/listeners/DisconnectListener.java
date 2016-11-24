8f3e2f3f43bab9b664b89b0087b9daaf30902c9f
xMalware <xmalware2@gmail.com>
2016-11-24 20:18:46 +0100

    y

--- src/fr/badblock/game/core18R3/listeners/DisconnectListener.java
+++ src/fr/badblock/game/core18R3/listeners/DisconnectListener.java
@@ -27,6 +27,7 @@ import fr.badblock.gameapi.game.GameServer.WhileRunningConnectionTypes;
 import fr.badblock.gameapi.game.GameState;
 import fr.badblock.gameapi.players.BadblockPlayer;
 import fr.badblock.gameapi.players.BadblockPlayer.BadblockMode;
+import fr.badblock.gameapi.players.data.PlayerData;
 import fr.badblock.gameapi.players.data.boosters.PlayerBooster;
 import fr.badblock.gameapi.run.RunType;
 import fr.badblock.gameapi.utils.general.Callback;
@@ -42,9 +43,11 @@ public class DisconnectListener extends BadListener {
 
 		if(GameAPI.getAPI().getRunType().equals(RunType.GAME) && GameAPI.getAPI().getGameServer().getGameState().equals(GameState.RUNNING) && GameAPI.getAPI().isLeaverBusterEnabled() && player.getBadblockMode() != BadblockMode.SPECTATOR && !player.hasPermission("api.leaverbuster.bypass")){
 			System.out.println("Added LeaverBuster for " + player.getName());
-			player.getPlayerData().getLeaves().add(System.currentTimeMillis());
+			PlayerData playerData = player.getPlayerData();
+			List<Long> leaves = playerData.getLeaves();
+			leaves.add(System.currentTimeMillis());
 			JsonObject jsonObject = new JsonObject();
-			jsonObject.add("leaves", GameAPI.getGson().toJsonTree(player.getPlayerData().getLeaves()));
+			jsonObject.add("leaves", GameAPI.getGson().toJsonTree(leaves));
 			GameAPI.getAPI().getLadderDatabase().updatePlayerData(player, jsonObject);
 			TaskManager.runTaskLater(new Runnable() {
 				@Override
