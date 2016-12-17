d7e2bfb7ae940249bba1320cc375689fdbfe8e90
xMalware <xmalware2@gmail.com>
2016-12-17 20:09:54 +0100

    fix

--- src/fr/badblock/game/core18R3/technologies/rabbitlisteners/VanishTeleportListener.java
+++ src/fr/badblock/game/core18R3/technologies/rabbitlisteners/VanishTeleportListener.java
@@ -10,7 +10,6 @@ import fr.badblock.gameapi.players.BadblockPlayer;
 import fr.badblock.gameapi.players.BadblockPlayer.BadblockMode;
 import fr.badblock.gameapi.run.RunType;
 import fr.badblock.gameapi.utils.BukkitUtils;
-import fr.badblock.gameapi.utils.threading.TaskManager;
 import fr.badblock.rabbitconnector.RabbitConnector;
 import fr.badblock.rabbitconnector.RabbitListener;
 import fr.badblock.rabbitconnector.RabbitListenerType;
@@ -26,20 +25,15 @@ public class VanishTeleportListener extends RabbitListener {
 
 	@Override
 	public void onPacketReceiving(String body) {
-		TaskManager.runTask(new Runnable() {
-			@Override
-			public void run() {
-				String[] splitter = body.split(";");
-				BadblockPlayer player = BukkitUtils.getPlayer(splitter[0]);
-				if (player == null) {
-					time.put(splitter[0].toLowerCase(), System.currentTimeMillis() + 10000);
-					splitters.put(splitter[0].toLowerCase(), splitter);
-				}
-				else {
-					manage(player, splitter);
-				}
-			}
-		});
+		String[] splitter = body.split(";");
+		BadblockPlayer player = BukkitUtils.getPlayer(splitter[0]);
+		if (player == null) {
+			time.put(splitter[0].toLowerCase(), System.currentTimeMillis() + 10000);
+			splitters.put(splitter[0].toLowerCase(), splitter);
+		}
+		else {
+			manage(player, splitter);
+		}
 	}
 
 	public static void manage(BadblockPlayer player, String[] splitter) {
