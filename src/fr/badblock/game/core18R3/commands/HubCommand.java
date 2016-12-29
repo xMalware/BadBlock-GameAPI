e8326c1c155f21b9ec4575628297b9477a4bca8d
xMalware <xmalware2@gmail.com>
2016-12-29 17:46:47 +0100

    Add hub command

---
+++ src/fr/badblock/game/core18R3/commands/HubCommand.java
@@ -0,0 +1,25 @@
+package fr.badblock.game.core18R3.commands;
+
+import org.bukkit.command.CommandSender;
+
+import fr.badblock.gameapi.command.AbstractCommand;
+import fr.badblock.gameapi.players.BadblockPlayer;
+import fr.badblock.gameapi.players.BadblockPlayer.GamePermission;
+import fr.badblock.gameapi.utils.i18n.TranslatableString;
+
+public class HubCommand extends AbstractCommand {
+	
+	public HubCommand() {
+		super("hub", new TranslatableString("commands.hub.usage"), GamePermission.PLAYER);
+		allowConsole(false);
+	}
+
+	@Override
+	public boolean executeCommand(CommandSender sender, String[] args) {
+		if(args.length == 0) return false;
+	
+		BadblockPlayer player = (BadblockPlayer) sender;
+		player.sendPlayer("lobby");
+		return true;
+	}
+}
