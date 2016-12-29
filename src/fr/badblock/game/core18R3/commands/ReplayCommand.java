3cbf1277bf9a740b9665139d856bbe7d1dba9e82
xMalware <xmalware2@gmail.com>
2016-12-29 17:22:19 +0100

    Add replay command

---
+++ src/fr/badblock/game/core18R3/commands/ReplayCommand.java
@@ -0,0 +1,31 @@
+package fr.badblock.game.core18R3.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
+import fr.badblock.gameapi.GameAPI;
+import fr.badblock.gameapi.command.AbstractCommand;
+import fr.badblock.gameapi.players.BadblockPlayer;
+import fr.badblock.gameapi.players.BadblockPlayer.GamePermission;
+import fr.badblock.gameapi.run.RunType;
+import fr.badblock.gameapi.sentry.SEntry;
+import fr.badblock.gameapi.utils.i18n.TranslatableString;
+
+public class ReplayCommand extends AbstractCommand {
+	
+	public ReplayCommand() {
+		super("replay", new TranslatableString("commands.replay.usage"), GamePermission.PLAYER);
+		allowConsole(false);
+	}
+
+	@Override
+	public boolean executeCommand(CommandSender sender, String[] args) {
+		if(args.length == 0) return false;
+		if (!GameAPI.getAPI().getRunType().equals(RunType.GAME)) return true;
+		
+		BadblockPlayer player = (BadblockPlayer) sender;
+		GameAPI.getAPI().getRabbitSpeaker().sendAsyncUTF8Publisher("networkdocker.sentry.join", GameAPI.getGson().toJson(new SEntry(player.getName(), Bukkit.getServerName().split("_")[0], true)), 5000, false);
+
+		return true;
+	}
+}
