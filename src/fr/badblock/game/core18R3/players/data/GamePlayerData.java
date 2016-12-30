79cb139da0f66f60d0ca8ccd2fa09b0fa9d3702a
xMalware <xmalware2@gmail.com>
2016-12-30 02:56:49 +0100

    r

--- src/fr/badblock/game/core18R3/players/data/GamePlayerData.java
+++ src/fr/badblock/game/core18R3/players/data/GamePlayerData.java
@@ -402,18 +402,18 @@ public class GamePlayerData implements PlayerData {
 		double multiplier = 1;
 		PlayerBooster playerBooster = GamePlugin.getInstance().getBooster();
 		if (playerBooster != null && playerBooster.getBooster() != null) {
-			multiplier += playerBooster.getBooster().getXpMultiplier();
+			multiplier += playerBooster.getBooster().getXpMultiplier() - 1;
 		}
 		multiplier += GameAPI.getAPI().getServerXpBonus() <= 0 ? 0 : (GameAPI.getAPI().getServerXpBonus() - 1);
-		double v = 1;
+		double v = 0;
 		try {
 			if (this.getGameBadblockPlayer() != null) {
 				Double o = this.getGameBadblockPlayer().getPermissionValue("xpboost", Double.class);
 				if (o == null) o = 1.0d;
-				v = o;
+				v = o - 1;
 			}
 		}catch(Exception error) {
-			v = 1;
+			v = 0;
 		}
 		multiplier += v;
 		return multiplier;
