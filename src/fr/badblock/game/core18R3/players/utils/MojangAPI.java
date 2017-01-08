54b4c2c929dd107bf12e0d4ce79b85dc83a72c8c
xMalware <xmalware2@gmail.com>
2017-01-08 04:24:24 +0100

    z

--- src/fr/badblock/game/core18R3/players/utils/MojangAPI.java
+++ src/fr/badblock/game/core18R3/players/utils/MojangAPI.java
@@ -41,21 +41,16 @@ public class MojangAPI
 				e += c;
 		if ((output.isEmpty() || output.length() < 4 || e.isEmpty() || e.length() < 4) && uuid != null)
 		{
-			System.out.println("A -- " + uuid);
 			output = readURL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid + "?unsigned=false");
 		}else{
 			String o = "";
 			for (char c : output.toCharArray())
 				if (Character.isDigit(c) || c == '_' || c == '-' || Character.isLetter(c))
 					o += c;
-			System.out.println("B -- '" + o + "' - '" + output + "'");
 			if (!o.isEmpty()) {
 				output = readURL("https://api.mojang.com/users/profiles/minecraft/" + o);
-				System.out.println("C -- " + output);
 				output = output.substring(7, 39);
-				System.out.println("D -- " + output);
 				output = readURL("https://sessionserver.mojang.com/session/minecraft/profile/" + output + "?unsigned=false");
-				System.out.println("E -- " + output);
 			}
 		}
 		value = getStringBetween(output, mid, valend);
