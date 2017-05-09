2831633bd44f90f2fa31039b40676e4753e10b62
Audran <Audran@DESKTOP-B45H8L4>
2017-05-09 21:02:32 +0200

    Better //set command

--- src/fr/badblock/game/core18R3/commands/we/SetCommand.java
+++ src/fr/badblock/game/core18R3/commands/we/SetCommand.java
@@ -1,6 +1,9 @@
 package fr.badblock.game.core18R3.commands.we;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.bukkit.Material;
@@ -9,30 +12,55 @@ import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 
 import fr.badblock.game.core18R3.worldedit.WorldEditThread;
 import fr.badblock.game.core18R3.worldedit.actions.WEActionSet;
+import fr.badblock.game.core18R3.worldedit.iterators.WECuboidIterator;
+import fr.badblock.game.core18R3.worldedit.iterators.WECylinderIterator;
+import fr.badblock.game.core18R3.worldedit.iterators.WEEllipsoidIterator;
+import fr.badblock.game.core18R3.worldedit.iterators.WEEmptyCuboidIterator;
+import fr.badblock.game.core18R3.worldedit.iterators.WEEmptyCylinderIterator;
+import fr.badblock.game.core18R3.worldedit.iterators.WEEmptyEllipsoidIterator;
 import fr.badblock.game.core18R3.worldedit.iterators.WELineIterator;
+import fr.badblock.game.core18R3.worldedit.iterators.WEWallsIterator;
 import fr.badblock.gameapi.GameAPI;
 import fr.badblock.gameapi.players.BadblockPlayer;
+import fr.badblock.gameapi.utils.general.StringUtils;
+import fr.badblock.gameapi.utils.selections.CuboidSelection;
 import fr.badblock.gameapi.worldedit.WEBlockIterator;
 import net.minecraft.server.v1_8_R3.Block;
 import net.minecraft.server.v1_8_R3.Item;
 import net.minecraft.server.v1_8_R3.MinecraftKey;
 
 public class SetCommand extends SelectionNeededCommand {
-	public SetCommand() {
-		super("set");
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	protected boolean exec(BadblockPlayer concerned, String[] args) {
-		if(args.length == 0)
-			return false;
+	private final Map<String, Class<?>> shapes;
+	
+	public static String[] splitItem(String item)
+	{
+		String[] splitted = item.split(":");
+		String resMaterial = "";
+		String resData = "0";
 		
-		Material material = Material.matchMaterial(args[0]);
-		byte data = 0;
-
+		int i = 0;
+		
+		if(splitted[i].equalsIgnoreCase("minecraft"))
+		{
+			i++;
+			resMaterial = "minecraft:";
+		}
+		
+		if(i < splitted.length)
+			resMaterial += splitted[i++];
+		
+		if(i < splitted.length)
+			resData = splitted[i++];
+		
+		return new String[]{resMaterial, resData};
+	}
+	
+	public static Material getItem(String from)
+	{
+		Material material = Material.matchMaterial(from);
+		
 		if(material == null) {
-			Item nmsItem = Item.REGISTRY.get(new MinecraftKey(args[0]));
+			Item nmsItem = Item.REGISTRY.get(new MinecraftKey(from));
 
 			if(nmsItem != null) {
 				net.minecraft.server.v1_8_R3.ItemStack localItemStack = new net.minecraft.server.v1_8_R3.ItemStack(nmsItem, 1);
@@ -40,27 +68,77 @@ public class SetCommand extends SelectionNeededCommand {
 			}
 		}
 		
+		return material;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static boolean isValidBlock(Material material, byte data)
+	{
+		return Block.d.a( (material.getId() << 4) | data ) != null;
+	}
+	
+	public SetCommand() {
+		super("set");
+		
+		shapes = new HashMap<>();
+		shapes.put("cuboid", WECuboidIterator.class);
+		shapes.put("hcuboid", WEEmptyCuboidIterator.class);
+		shapes.put("walls", WEWallsIterator.class);
+		shapes.put("sphere", WEEllipsoidIterator.class);
+		shapes.put("hsphere", WEEmptyEllipsoidIterator.class);
+		shapes.put("cyl", WECylinderIterator.class);
+		shapes.put("hcyl", WEEmptyCylinderIterator.class);
+		shapes.put("line", WELineIterator.class);
+	}
+
+	@Override
+	protected boolean exec(BadblockPlayer concerned, String[] args) {
+		if(args.length == 0)
+			return false;
+		
+		String[] splitted = splitItem(args[0]);
+		
+		Material material = getItem(splitted[0]);
+		byte data = 0;
+		WEBlockIterator iterator;
+		
 		if(material == null) {
 			sendTranslatedMessage(concerned, "commands.give.unknow-type", args[0]);
 			return true;
 		}
 
 		try {
-			if(args.length > 1) {
-				data = Byte.parseByte(args[1]);
-			}
+			data = Byte.parseByte(splitted[1]);
 		} catch (Exception e) {
 			sendTranslatedMessage(concerned, "commands.nan", args[1]);
 			return true;
 		}
 		
-		if(Block.d.a( (material.getId() << 4) | data ) == null)
+		if(!isValidBlock(material, data))
 		{
-			sendTranslatedMessage(concerned, "commands.worldedit.invalidblock", args[0], args.length == 1 ? 0 : args[1]);
+			sendTranslatedMessage(concerned, "commands.worldedit.invalidblock", splitted[0] + ":" + splitted[1]);
 			return true;
 		}
 		
-		WEBlockIterator iterator = new WELineIterator(concerned.getSelection());
+		if(args.length > 1)
+		{
+			String shape = args[1].toLowerCase();
+			
+			if(!shapes.containsKey(shape))
+			{
+				sendTranslatedMessage(concerned, "commands.worldedit.invalidshape", args[1], StringUtils.join(shapes.keySet(), "&b, ", "&7, ") );
+				return true;
+			}
+			
+			try {
+				iterator = (WEBlockIterator)shapes.get(shape).getConstructor(CuboidSelection.class).newInstance(concerned.getSelection());
+			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+				return true;
+			}
+		}
+		else iterator = new WECuboidIterator(concerned.getSelection());
+		
 		WEActionSet action = new WEActionSet(iterator, concerned, material, data);
 		WorldEditThread thread = new WorldEditThread();
 		
@@ -85,6 +163,11 @@ public class SetCommand extends SelectionNeededCommand {
 			return Item.REGISTRY.keySet().stream().map(mcKey -> {
 				return mcKey.toString();
 			}).collect(Collectors.toList());
+		}
+		else if(args.length == 3)
+		{
+			args[2] = args[2].toLowerCase();
+			return shapes.keySet().stream().collect(Collectors.toList());
 		}
 		
 		return null;
