09cd93b02439e5effdfb63cdcc4023d7d9b563c8
LeLanN <audran.doublet@epita.fr>
2017-05-14 18:35:28 +0200

    SetTextureProperty

--- src/fr/badblock/game/core18R3/players/GameBadblockPlayer.java
+++ src/fr/badblock/game/core18R3/players/GameBadblockPlayer.java
@@ -266,10 +266,10 @@ public class GameBadblockPlayer extends CraftPlayer implements BadblockPlayer {
 		}
 	}
 
-    @Override
-    public EntityPlayer getHandle() {
-        return (EntityPlayer) entity;
-    }
+	@Override
+	public EntityPlayer getHandle() {
+		return (EntityPlayer) entity;
+	}
 
 	private GameAPI getAPI() {
 		return GameAPI.getAPI();
@@ -1234,55 +1234,69 @@ public class GameBadblockPlayer extends CraftPlayer implements BadblockPlayer {
 
 	@Override
 	public void setPlayerSkin(String skinUrl) {
-        setPlayerSkin0(String.format("{textures:{SKIN:{url:\"%s\"}}}", skinUrl));
+		setPlayerSkin0(String.format("{textures:{SKIN:{url:\"%s\"}}}", skinUrl));
 	}
 
 	@Override
 	public void setPlayerSkin(String skinUrl, String capeUrl) {
 		setPlayerSkin0(String.format("{textures:{SKIN:{url:\"%s\"},CAPE:{url\"%s\"}}", skinUrl, capeUrl));
 	}
-	
+
 	private void setPlayerSkin0(String textures)
 	{
 		PropertyMap map = this.getHandle().getProfile().getProperties();
-		
+
 		map.clear();
-		
+
 		byte[] encodedData = Base64.getEncoder().encode(textures.getBytes());
-        map.put("textures", new Property("textures", new String(encodedData)));
-        
-        resetPlayerView();
-        
-        for(BadblockPlayer player : GameAPI.getAPI().getOnlinePlayers())
-        {
-        	if(player == this || !player.canSee(this))
-        		return;
-        	
-        	player.hidePlayer(this);
-        	player.showPlayer(this);
-        }
+		map.put("textures", new Property("textures", new String(encodedData)));
+
+		updatePlayerSkin();
 	}
-	
+
+	@Override
+	public void setTextureProperty(String value, String signature) {
+		PropertyMap map = this.getHandle().getProfile().getProperties();
+
+		map.clear();
+		map.put("textures", new Property("textures", value, signature));
+		updatePlayerSkin();
+	}
+
+	public void updatePlayerSkin()
+	{
+		resetPlayerView();
+
+		for(BadblockPlayer player : GameAPI.getAPI().getOnlinePlayers())
+		{
+			if(player == this || !player.canSee(this))
+				return;
+
+			player.hidePlayer(this);
+			player.showPlayer(this);
+		}
+	}
+
 	public void resetPlayerView()
 	{
 		Environment env = customEnvironment == null ? getWorld().getEnvironment() : customEnvironment;
-		
-		
-        getAPI().createPacket(PlayPlayerInfo.class).setAction(TabAction.REMOVE_PLAYER).addPlayer(getUniqueId(), createPlayerInfo()).send(this);
-        getAPI().createPacket(PlayPlayerInfo.class).setAction(TabAction.ADD_PLAYER).addPlayer(getUniqueId(), createPlayerInfo()).send(this);
+
+
+		getAPI().createPacket(PlayPlayerInfo.class).setAction(TabAction.REMOVE_PLAYER).addPlayer(getUniqueId(), createPlayerInfo()).send(this);
+		getAPI().createPacket(PlayPlayerInfo.class).setAction(TabAction.ADD_PLAYER).addPlayer(getUniqueId(), createPlayerInfo()).send(this);
 
 		getAPI().createPacket(PlayRespawn.class).setDimension(env).setDifficulty(getWorld().getDifficulty()).setGameMode(getGameMode()).setWorldType(getWorld().getWorldType()).send(this);
-		
+
 		updateInventory();
 
 		updateScaledHealth();
 		getHandle().triggerHealthUpdate();
-		
+
 		getInventory().setHeldItemSlot(getInventory().getHeldItemSlot());
 		setItemInHand(getItemInHand());
 		getHandle().updateAbilities();
 	}
-	
+
 	private PlayerInfo createPlayerInfo()
 	{
 		return new PlayerInfo(getUniqueId(), getName(), getHandle().getProfile().getProperties(), getGameMode(), getPing(), getDisplayName());
