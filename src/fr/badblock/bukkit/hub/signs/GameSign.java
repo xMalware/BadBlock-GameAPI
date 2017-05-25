5e4ec2935f882b8a7ca171a3f8283624ef9d46d9
xMalware <xmalware2@gmail.com>
2017-05-25 12:37:41 +0200

    t

---
+++ src/fr/badblock/bukkit/hub/signs/GameSign.java
@@ -0,0 +1,18 @@
+package fr.badblock.bukkit.hub.signs;
+
+import java.util.List;
+
+public class GameSign {
+
+	public String 		name;
+	public String[]     sentries;
+	
+	public GameSign() {
+		
+	}
+	
+	public void receive() {
+		
+	}
+	
+}
