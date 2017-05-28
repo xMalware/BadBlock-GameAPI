4f0f02522d62edb34e19fca2effc2120e41b21cf
xMalware <xmalware2@gmail.com>
2017-05-28 17:09:22 +0200

    fix

--- src/fr/badblock/bukkit/hub/inventories/selector/animhost/items/Tower2v2SelectorItem.java
+++ src/fr/badblock/bukkit/hub/inventories/selector/animhost/items/Tower2v2SelectorItem.java
@@ -5,7 +5,7 @@ import fr.badblock.gameapi.run.BadblockGame;
 public class Tower2v2SelectorItem extends AnimHostItem {
 
 	public Tower2v2SelectorItem() {
-		super("hub.items.towerselectoritem.2v2", BadblockGame.TOWER, "tower2v2_tower");
+		super("hub.items.towerselectoritem.2v2", BadblockGame.TOWER, "tower");
 	}
 
 }
