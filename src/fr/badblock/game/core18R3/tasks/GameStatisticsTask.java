5ccdbb1cf031db65662beafe37f862d4be7ef318
xMalware <xmalware2@gmail.com>
2017-05-31 12:03:28 +0200

    fix

--- src/fr/badblock/game/core18R3/tasks/GameStatisticsTask.java
+++ src/fr/badblock/game/core18R3/tasks/GameStatisticsTask.java
@@ -1,18 +1,23 @@
 package fr.badblock.game.core18R3.tasks;
 
 import java.sql.ResultSet;
+import java.util.Collection;
+import java.util.Iterator;
 
 import fr.badblock.gameapi.GameAPI;
 import fr.badblock.gameapi.databases.SQLRequestType;
 import fr.badblock.gameapi.game.GameState;
+import fr.badblock.gameapi.players.BadblockTeam;
 import fr.badblock.gameapi.utils.BukkitUtils;
 import fr.badblock.gameapi.utils.general.Callback;
 import fr.badblock.gameapi.utils.threading.TaskManager;
 
 public class GameStatisticsTask implements Runnable {
 
+	private int		  tick;
 	private GameState lastState;
 	private int		  startedGamePlayers;
+	private boolean	  fullAliveTeams;
 	
 	public GameStatisticsTask() {
 		TaskManager.scheduleSyncRepeatingTask("gameStatisticsTask", this, 1, 1);
@@ -20,15 +25,16 @@ public class GameStatisticsTask implements Runnable {
 	
 	@Override
 	public void run() {
+		tick++;
 		GameState state = GameAPI.getAPI().getGameServer().getGameState();
+		int gamePlayers = BukkitUtils.getPlayers().size();
 		if (lastState != null) {
 			if (lastState.equals(GameState.RUNNING) && state.equals(GameState.FINISHED)) {
-				int gamePlayers = BukkitUtils.getPlayers().size();
 				if (gamePlayers < 2) {
 					// Pas assez de joueurs
 					increment(true, true);
 				}else{
-					if (gamePlayers < startedGamePlayers) {
+					if (!fullAliveTeams) {
 						// Moins de joueurs qu'au démarrage de la partie
 						increment(true, false);
 					}else{
@@ -38,8 +44,26 @@ public class GameStatisticsTask implements Runnable {
 				}
 			}else if (lastState.equals(GameState.WAITING) && state.equals(GameState.RUNNING)) {
 				startedGamePlayers = BukkitUtils.getPlayers().size();
+			}else if (state.equals(GameState.RUNNING) && lastState.equals(GameState.RUNNING)) {
+				if (tick == 20) {
+					Collection<BadblockTeam> collection = GameAPI.getAPI().getTeams();
+					if (collection.size() == 0) fullAliveTeams = gamePlayers >= startedGamePlayers; // Pas de système de team
+					else {
+						boolean tempFull = true;
+						Iterator<BadblockTeam> iterator = collection.iterator();
+						while (iterator.hasNext()) {
+							BadblockTeam team = iterator.next();
+							if (team != null && !team.isDead()) {
+								if (team.getOnlinePlayers().size() < team.getMaxPlayers()) 
+									tempFull = false;
+							}
+						}
+						fullAliveTeams = tempFull;
+					}
+				}
 			}
 		}
+		if (tick == 20) tick = 0;
 		lastState = state;
 	}
 	
