32d8228ffefc905730bcceeff13777c53761be58
xMalware <xmalware2@gmail.com>
2017-11-19 23:00:07 +0100

    Auto replay

--- src/fr/badblock/game/core18R3/commands/AutoReplayCommand.java
+++ src/fr/badblock/game/core18R3/commands/AutoReplayCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.command.CommandSender;
 
 import fr.badblock.gameapi.GameAPI;
 import fr.badblock.gameapi.command.AbstractCommand;
+import fr.badblock.gameapi.players.BadblockPlayer;
 import fr.badblock.gameapi.players.BadblockPlayer.GamePermission;
 import fr.badblock.gameapi.run.RunType;
 import fr.badblock.gameapi.utils.i18n.TranslatableString;
@@ -18,10 +19,20 @@ public class AutoReplayCommand extends AbstractCommand {
 	@Override
 	public boolean executeCommand(CommandSender sender, String[] args) {
 		if (!GameAPI.getAPI().getRunType().equals(RunType.GAME)) return true;
+
+		BadblockPlayer player = (BadblockPlayer) sender;
 		
-		//BadblockPlayer player = (BadblockPlayer) sender;
+		if (args.length == 0)
+		{
+			player.getPlayerData().setReplay("");
+			player.sendTranslatedMessage("game.removereplay");
+			return true;
+		}
 		
-		// TODO
+		player.getPlayerData().setReplay(args[0]);
+		player.sendTranslatedMessage("game.setreplay", args[0]);
+		
+		player.saveGameData();
 		
 		return true;
 	}
